05a1f935c8c5d442f4c8cd06b7311d93c20ed43c
Cryobyte56 <danielasanon54@gmail.com>
2025-03-31 15:40:44 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+return (
+  <div className="p-10">
+    <h1 className="font-bold text-red-500 bg-yellow-300 border-4 border-black">
+      Hello, Tailwind!
+    </h1>
+    <h2 className="font-semibold text-green-500">Subheading</h2>
+    <p className="text-lg text-gray-700">This is a paragraph.</p>
+  </div>
+);
+}
+
+export default App;
